70d837719ff2fe8a98421cb1007be4e73d72f7f5
uri-ustrell <43264089+uri-ustrell@users.noreply.github.com>
2019-03-09 10:01:54 +0100

    Create extensions.sh

---
+++ VScode/extensions.sh
@@ -0,0 +1,16 @@
+
+code --install-extension Anjali.clipboard-history
+
+code --install-extension CoenraadS.bracket-pair-colorizer
+
+code --install-extension dbaeumer.vscode-eslint
+
+code --install-extension eamodio.gitlens
+
+code --install-extension eg2.tslint
+
+code --install-extension felixfbecker.php-intellisense
+
+code --install-extension ikappas.phpcs
+
+code --install-extension oderwat.indent-rainbow
